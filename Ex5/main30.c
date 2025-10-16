576e15c34b5ad70f284b0717091fc70b94428258
wsh20040921-hue <wsh20040921@gmail.com>
2025-10-16 15:41:09 +0800

    Add my_strlen function to calculate string length

---
+++ Ex5/main30.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int my_strlen(char* s) {
+    if (!*s) return 0;           
+    return 1 + my_strlen(s + 1);
+}
+
+int main() {
+    char s[] = "IU is a girl!";
+    printf("(%s) len = %d\n", s, my_strlen(s));
+    return 0;
+}
